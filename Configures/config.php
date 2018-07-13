ed3f2f0e21b17a9a1b4feb42d0aff688cf5a1759
zengxj <807750811@qq.com>
2018-07-13 21:09:18 +0800

    debugging

--- Configures/config.php
+++ Configures/config.php
@@ -3,8 +3,8 @@
 # Mysql配置参数
 define('MYSQL_HOST', '127.0.0.1');
 define('MYSQL_PORT', 3306);
-define('MYSQL_USER', 'mysqluser');
-define('MYSQL_AUTH', 'aide123456');
+define('MYSQL_USER', 'root');
+define('MYSQL_AUTH', 'zxj1105511101');
 define('MYSQL_DBNAME', 'wechat');
 
 # Redis配置参数
